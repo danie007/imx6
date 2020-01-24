568dafd37ba46dd423d505d6ab94f621e75bc4b5
Daniel Selvan D <danilselvan@gmail.com>
2020-01-24 11:01:53 +0530

    Update build_uboot.sh

--- build_uboot.sh
+++ build_uboot.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+echo ""
+echo "build_uboot.sh"
+echo ""
+
 DEFCONF=mx6ul_14x14_evk_defconfig
+toolchain=~/tools/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+
 LOG_FILE=build_$(date '+%y%^b%d_%H%M%S').log # output log format: build_yymmmdd_HHMMSS.log
 
 if [ $# -eq 0 ]; then
@@ -18,9 +24,14 @@ else
     make O=$1 distclean
     make O=$1 mrproper
     make O=$1 $DEFCONF
-    make O=$1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- 2>&1 | tee $1/$LOG_FILE
+    echo ""
+    echo "Select ARM architecture -> Support i.MX HAB features"
+    echo ""
+    make O=$1 ARCH=arm CROSS_COMPILE=$toolchain menuconfig
+    make O=$1 ARCH=arm CROSS_COMPILE=$toolchain 2>&1 | tee $1/$LOG_FILE
 
     echo ""
 
+    Echo "u-boot dump:"
     od -X -N 0x20 $1/u-boot-dtb.imx
 fi
