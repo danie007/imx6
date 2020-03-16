eafd7eed7cb14339a92bd608fa2a4428f02a8399
Daniel Selvan D <danilselvan@gmail.com>
2020-03-16 15:32:46 +0530

    Update build_uboot.sh

--- build_uboot.sh
+++ build_uboot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Filename: build_uboot_new.sh
+# Filename: build_uboot.sh
+# Description: Build script for uboot-imx 
+#              git clone https://source.codeaurora.org/external/imx/uboot-imx -b imx_v2019.04_4.19.35_1.1.0
 # Author: Daniel Selvan
+# Updated on Mar 16 2020
 
 ############################ USAGE ######################################
 #                                                                       #
-#   bash build_uboot_new.sh build_directory [build_option]              #
+#   bash build_uboot.sh build_directory [build_option]                  #
 #                                                                       #
 #   build_option:                                                       #
 #   -s, --secure    Support i.MX HAB features                           #
@@ -18,10 +21,15 @@
 
 DEFCONF=mx6ul_14x14_evk_defconfig
 toolchain=~/tools/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- # ARM Cross compiler
+err_str=Error                                                                             # Error keyword to be searched in the log file.
+
+reqcc=arm-linux-gnueabihf-gcc
+requiredver="6.2.0"
 
 LOG_FILE=$1/build_$(date '+%y%^b%d_%H%M%S').log # output log format: build_yymmmdd_HHMMSS.log
 boot_mode="default"
 
+GREEN='\e[1;32m'
 YELLOW='\e[1;33m'
 RED='\e[1;31m'
 NC='\e[0m'
@@ -49,12 +57,43 @@ elif [ "$(printf '%s' "$1" | cut -c1)" != "-" ]; then
     -e | --encrypt)
         boot_mode="encrypted"
         ;;
+    " " | "") ;;
+
     esac
+
 else
     usage
 fi
 
-echo "Boot mode: $boot_mode"
+echo -n "Checking dependencies... "
+for dep in make bison flex; do
+    [[ $(which $dep 2>/dev/null) ]] || {
+        echo -en "\n${YELLOW}$dep needs to be installed${NC}. Use 'sudo apt-get install $dep'"
+        deps=1
+    }
+done
+[[ $deps -ne 1 ]] && echo "OK" || {
+    echo -en "\nInstall the above and rerun this script\n"
+    exit 1
+}
+
+echo -n "Checking cross compiler... "
+cc="$(${toolchain}gcc --version | head -n 1 | cut -d " " -f1)"
+currentver="$(${toolchain}gcc -dumpversion 2>/dev/null)"
+if [ "$currentver" == "" ]; then
+    echo -e "\n${YELLOW}Kindly check the toolchain path or update the script${NC}"
+    CC=1
+elif [ "$(printf '%s\n' "$cc")" != "$reqcc" ]; then
+    echo -e "\n${YELLOW}No Compatible Linux hosted cross compiler found.${NC}"
+    CC=1
+elif [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
+    echo -e "\n${YELLOW}Kindly update your cross compiler version to minimum $requiredver${NC}"
+    CC=1
+fi
+[[ $CC -ne 1 ]] && echo "OK" || {
+    echo -en "\nFix cross compiler and rerun this script\n"
+    exit 1
+}
 
 make distclean
 make mrproper
@@ -65,7 +104,7 @@ if [ -d $1 ]; then
     rm -rf $1
 fi
 
-make O=$1 ARCH=arm CROSS_COMPILE=$toolchain distclean 2>&1 | tee -a $LOG_FILE
+make O=$1 ARCH=arm CROSS_COMPILE=$toolchain distclean
 make O=$1 ARCH=arm CROSS_COMPILE=$toolchain mrproper 2>&1 | tee -a $LOG_FILE
 make O=$1 ARCH=arm CROSS_COMPILE=$toolchain $DEFCONF 2>&1 | tee -a $LOG_FILE
 
@@ -178,29 +217,40 @@ EOT
     patch -u -b $1/.config -i secure.patch 2>&1 | tee -a $LOG_FILE
     rm -f secure.patch
 fi
-make -j4 O=$1 ARCH=arm CROSS_COMPILE=$toolchain 2>&1 | tee -a $LOG_FILE
+make O=$1 ARCH=arm CROSS_COMPILE=$toolchain -j$(nproc) 2>&1 | tee -a $LOG_FILE
+
+# Checking for build errors
+if grep -q $err_str $LOG_FILE; then
+    echo -e "\n${RED}Build error has occurred. Look into $LOG_FILE for more details.${NC}\n" 2>&1 | tee -a $LOG_FILE
+    exit 1
+fi
 
 echo "" 2>&1 | tee -a $LOG_FILE
-echo "U-Boot dump:" 2>&1 | tee -a $LOG_FILE
+echo -e "${GREEN}*** U-Boot dump ***${NC}" 2>&1 | tee -a $LOG_FILE
 # od -X -N 0x20 $1/u-boot-dtb.imx
 
-echo "IVT Header: 0x$(hexdump -e '/4 "%X""\n"' -s 0 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
+echo "IVT Header:         0x$(hexdump -e '/4 "%X""\n"' -s 0 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
 echo "U-Boot entry point: 0x$(hexdump -e '/4 "%X""\n"' -s 4 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
-echo "DCD PTR: 0x$(hexdump -e '/4 "%X""\n"' -s 12 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
-echo "Boot Data PTR: 0x$(hexdump -e '/4 "%X""\n"' -s 16 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
+echo "DCD PTR:            0x$(hexdump -e '/4 "%X""\n"' -s 12 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
+echo "Boot Data PTR:      0x$(hexdump -e '/4 "%X""\n"' -s 16 -n 4 $1/u-boot-dtb.imx)" 2>&1 | tee -a $LOG_FILE
 
 IVT_SELF=$(hexdump -e '/4 "%X""\n"' -s 20 -n 4 $1/u-boot-dtb.imx)
-echo "IVT Self Address: 0x$IVT_SELF" 2>&1 | tee -a $LOG_FILE
+echo "IVT Self Address:   0x$IVT_SELF" 2>&1 | tee -a $LOG_FILE
 
 CSF_PTR=$(hexdump -e '/4 "%X""\n"' -s 24 -n 4 $1/u-boot-dtb.imx)
-echo "CSF PTR: 0x$CSF_PTR" 2>&1 | tee -a $LOG_FILE
+echo "CSF PTR:            0x$CSF_PTR" 2>&1 | tee -a $LOG_FILE
 
 echo "" 2>&1 | tee -a $LOG_FILE
-IMG_LEN=$(printf '%X\n' $((0x$CSF_PTR - 0x$IVT_SELF)))
-echo "Image length: CSF PTR – IVT Self = 0x$IMG_LEN" 2>&1 | tee -a $LOG_FILE
+if [ "$boot_mode" == "default" ]; then
+    echo "Image length:       0x$(hexdump $1/u-boot-dtb.imx | tail -n 1)" 2>&1 | tee -a $LOG_FILE
+else
+    IMG_LEN=$(printf '%X\n' $((0x$CSF_PTR - 0x$IVT_SELF)))
+    echo "Image length: CSF PTR – IVT Self = 0x$IMG_LEN" 2>&1 | tee -a $LOG_FILE
+fi
 
-mkdir $1/_build_files
-mv $1/* $1/_build_files/ 2>/dev/null
-mv $1/_build_files/source $1/ && mv $1/_build_files/*.log $1/ && mv $1/_build_files/*.imx $1/ && mv $1/_build_files/*.sh $1/ 2>/dev/null
+mkdir $1/build_files
+mv $1/.* $1/build_files/ 2>/dev/null
+mv $1/* $1/build_files/ 2>/dev/null # Moving all files to the build_files directory
+mv $1/build_files/*.log $1/ && mv $1/build_files/*.imx $1/ && mv $1/build_files/*.sh $1/ 2>/dev/null
 cp $1/u-boot-dtb.imx $1/u-boot-dtb.imx.orig
 echo ""
