0cd011193054e65c2c37314b3ee5e685c9c378d7
danie007 <danilselvan@gmail.com>
2020-04-29 12:47:27 +0530

    Added condition to check Recmommended space(120 GB)

--- yocto_req_check.sh
+++ yocto_req_check.sh
@@ -4,19 +4,18 @@
 # Description: Script to check Yocto project requirements
 # 
 # Author: Daniel Selvan
-# Updated on Apr 28 2020
+# Updated on Apr 29 2020
 
-############################ USAGE ######################################
-#                                                                       #
-#   bash yocto_req_check.sh               #
-#                                                                       #
-#   build_option:                                                       #
-#   TODO              #
-#                                                                       #
-#########################################################################
+############################ USAGE ############################
+#                                                             #
+#   sudo bash yocto_req_check.sh                              #
+#                                                             #
+###############################################################
 
-k_OS_MIN_VERSION=14   # Minimum supported version in Ubuntu
-k_MIN_SPACE=50200000  # 50.2GB in KB
+k_OS_MIN_VERSION=16     # Minimum supported version in Ubuntu
+k_RECOM_SPACE=120000000 # 120 GB in KB - REcommended Space in Ubuntu 
+k_MIN_SPACE=50200000    # 50.2GB in KB - Minumum required space
+k_KB_TO_GB_CON=1000000  # Constant to convert KB to GB (KB/1000000)
 # k_MIN_SPACE=20200000  # Testing - 20.2GB in KB
 
 # Dependiences of Yocto build
@@ -89,13 +88,20 @@ else
     echo "Kindly check https://www.yoctoproject.org/docs/3.1/ref-manual/ref-manual.html#detailed-supported-distros for $OS_NAME support before proceed"
 fi
 
+# Getting available space in disk
+available_space=$(df -Pk . | awk 'NR==2 {print $4}')
+
 # Checking for minimum space requirements
-if [ "$(df -Pk . | awk 'NR==2 {print $4}')" -lt $k_MIN_SPACE ]; then
+if [ "$available_space" -lt $k_MIN_SPACE ]; then
     # ERR
     # TODO colorise
-    echo "50 GB is not available in current disk ($(df -Pk . | awk 'NR==2 {print $1}'))"
+    echo "$(($k_MIN_SPACE/$k_KB_TO_GB_CON))GB is not available in current disk ($(df -Pk . | awk 'NR==2 {print $1}'))"
     echo "Update storage or try in different disk to proceed"
     exit $ES_NO_SPC_ERR
+elif [ "$available_space" -lt $k_RECOM_SPACE ]; then
+    # Warning
+    # TODO colorise
+    echo "It is recommended to have $(($k_RECOM_SPACE/$k_KB_TO_GB_CON))GB for hassle free build, but $available_space is just sufficient"
 fi
 
 update_depen=()
@@ -155,99 +161,8 @@ if [ "$depen_count" -ne 0 ];then
         done
 
         echo "Enable internet or manually install the dependencies to continue"
-
         exit $ES_UNMET_DEPEN_ERR
     fi
 fi
-
-# DEFCONF=mx6ul_14x14_evk_defconfig
-# toolchain=~/tools/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf- # ARM Cross compiler
-# err_str=Error   # Error keyword to be searched in the log file.
-
-# reqcc=arm-linux-gnueabihf-gcc
-# requiredver="6.2.0"
-
-# LOG_FILE=$1/build_$(date '+%y%^b%d_%H%M%S').log # output log format: build_yymmmdd_HHMMSS.log
-# boot_mode="default"
-
-# GREEN='\e[1;32m'
-# YELLOW='\e[1;33m'
-# RED='\e[1;31m'
-# NC='\e[0m'
-
-# usage() {
-#     echo -e "\n${RED}Usage: $0 build_directory [build_option]${NC}\n"
-#     echo "build_option:"
-#     echo "-s, --secure    Support i.MX HAB features"
-#     echo "-e, --encrypt   Support the 'dek_blob' command"
-#     echo "If no option specified build the default configuration of U-boot"
-#     echo ""
-#     echo "-h, --help      Displays this help message and exit"
-
-#     exit 1
-# }
-
-# # Argument Parser
-# if [ $# -eq 0 ]; then
-#     usage
-# # 1st paramater is mandatory, 2nd parameter is optional
-# elif [ "$(printf '%s' "$1" | cut -c1)" != "-" ]; then
-#     case $2 in
-#     -s | --secure)
-#         boot_mode="secured"
-#         ;;
-#     -e | --encrypt)
-#         boot_mode="encrypted"
-#         ;;
-#     " " | "") ;;
-#         # If second paramter is not present, pass
-#     *)
-#         usage
-#         ;;
-#     esac
-# else
-#     usage
-# fi
-
-# echo -e "${YELLOW}**WARNING** Script contains hard coded file names/directories, update them before execution.${NC}"
-
-# echo -n "Checking dependencies... "
-# for dep in make bison flex; do
-#     [[ $(which $dep 2>/dev/null) ]] || {
-#         echo -en "\n${YELLOW}$dep needs to be installed${NC}. Use 'sudo apt-get install $dep'"
-#         deps=1
-#     }
-# done
-# [[ $deps -ne 1 ]] && echo "OK" || {
-#     echo -en "\nInstall the above and rerun this script\n"
-#     exit 1
-# }
-
-# echo -n "Checking cross compiler... "
-# cc="$(${toolchain}gcc --version | head -n 1 | cut -d " " -f1)"
-# currentver="$(${toolchain}gcc -dumpversion 2>/dev/null)"
-# if [ "$currentver" == "" ]; then
-#     echo -e "\n${YELLOW}Kindly check the toolchain path or update the script${NC}"
-#     CC=1
-# elif [ "$(printf '%s\n' "$cc")" != "$reqcc" ]; then
-#     echo -e "\n${YELLOW}No Compatible Linux hosted cross compiler found.${NC}"
-#     CC=1
-# elif [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
-#     echo -e "\n${YELLOW}Kindly update your cross compiler version to minimum $requiredver${NC}"
-#     CC=1
-# fi
-# [[ $CC -ne 1 ]] && echo "OK" || {
-#     echo -en "\nFix cross compiler and rerun this script\n"
-#     exit 1
-# }
-
-# make distclean
-# make mrproper
-
-# # Deletes the existing dir, if present
-# if [ -d $1 ]; then
-#     echo "$1 already present, removing..."
-#     rm -rf $1
-# fi
 
 exit $ES_SUCCESS
